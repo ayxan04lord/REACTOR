7ef5480bb72a77b6c37bb3927194f59e26714212
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2024-05-14 01:28:18 +0400

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import './header.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+
+  const qeydiyyat = false;
+  let Text = ""
+
+  if(qeydiyyat){
+    Text = "Çıxış 1"
+  }else{
+    Text = "Giriş 2"
+  }
+  
+    const Vaqif = () => {
+      return (
+        <div className='elmeddin'>
+          <h1>Hello</h1>
+          <p>Lesson 1</p>
+        </div>
+      )
+    }
+
+    const Murad = () => {
+      return (
+        <div className="main"></div>
+      )
+    }
+
+
+    const Ayxan = () => {
+ 
+        return (
+          <div className="ayxan_form">
+            <label htmlFor="yash">Yashinizi daxil edin</label>
+            <input type="text"
+              placeholder='Yawinizi daxil edin'
+              id='yash'
+              style={{
+                width: "200px",
+                height: "50px",
+                color: "green"
+              }} />
+            <button>{Text}</button>
+          </div>
+        )
+      
+    }
+
+    return (
+      <div className="demo">
+        <Murad />
+        <Vaqif />
+        <Murad />
+        <Murad />
+        <Ayxan />
+      </div>
+    )
+  }
+
+
+
 
 export default App;
