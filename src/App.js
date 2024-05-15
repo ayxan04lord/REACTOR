9f3437984283cdb3dbc080ad67cee719065301b6
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2024-05-15 22:08:05 +0400

    state with classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react'
+import { Component } from 'react'
 import './App.css'
 import './header.css'
 
-const App = () => {
 
-  const WhoAmI = ({name, surname, link, text}) => {
+
+
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      text: "super",
+      position: "Soz daxil edin",
+      bg: "",
+      color: ""
+    }
+  }
+
+  kolbasa = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+      text: "hello"
+    }))
+  }
+
+  changeText = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { count, text, position, color } = this.state;
+
     return (
-      <div className='card'>
-        <h3>Menim adim {name()}, soyadim {surname}.</h3>
-        <a href={link}>{text}</a>
+      <div className='card' style={{ background: color }}>
+        <button onClick={this.kolbasa}>{text}</button>
+        <button onClick={() => this.setState({ color: 'red' })}>change red</button>
+        <button onClick={() => this.setState({ color: 'green' })}>change green</button>
+        <button onClick={() => this.setState({ color: 'blue' })}>change blue</button>
+        <h1>
+          My name is {name}, surname is {surname}. <br /> Klik: {count}.
+        </h1>
+        <a href={link}>Profile</a>
+        <input type="text" onChange={this.changeText} />
+        <p>Text: {position}</p>
       </div>
     )
   }
+}
 
+function App() {
   return (
     <div>
-      <WhoAmI name={()=>{return "Rza"}} surname="Talibov" link="https://sethub.az" text="Social network"/>
-      <WhoAmI name={()=> {return "Ayxan"}} surname="Mustafayev" link="https://ayxan.com" text="Saytim" />
-      <WhoAmI name={()=>{ return "Vaqif"}} surname="Huseynov" link="https://Vaqif.az" text="Sport" />
-
+      <WhoAmI name="Rza" surname="Talibov" link="www.arrow.az" />
+      <WhoAmI name="Vaqif" surname="Huseynov" link="www.vaqif.az" />
     </div>
   )
-
 }
+
 export default App;
