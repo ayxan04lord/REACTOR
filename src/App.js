550cbc3a4f57f268d00cecf59f8f16ebaa786610
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2024-09-08 22:41:27 +0400

    add new changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import { Component } from 'react'
-import './App.css'
-import './header.css'
-import batman from './img/batman.jpg'
+import { Component } from 'react';
+import './App.css';
+import './header.css';
+import batman from './img/batman.jpg';
 
 class WhoAmI extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       count: 0,
       text: "super",
       position: "Soz daxil edin",
       bg: "",
       color: ""
-    }
+    };
   }
-
-  c
-
-  
-  
 
   yazi = () => {
     this.setState((state) => ({
       count: state.count + 1,
       text: "hello"
-    }))
+    }));
   }
 
   changeBgImg = () => {
     const { position } = this.state;
-    if (position === "batman") {
+    if (position.toLowerCase() === "batman") {
       this.setState({
         bg: `url(${batman})`
-      })
+      });
+    } else {
+      this.setState({
+        bg: ""
+      });
     }
   }
 
   changeText = (e) => {
     this.setState({
       position: e.target.value
-    })
+    });
   }
 
   render() {
@@ -56,11 +55,11 @@ class WhoAmI extends Component {
           My name is {name}, surname is {surname}. <br /> Klik: {count}.
         </h1>
         <a href={link}>Profile</a>
-        <input type="text" onChange={this.changeBgImg} />
+        <input type="text" onChange={this.changeText} placeholder="Enter 'batman'" />
         <button onClick={this.changeBgImg}>Sekil Deyis</button>
         <p>Text: {position}</p>
       </div>
-    )
+    );
   }
 }
 
@@ -70,7 +69,7 @@ function App() {
       <WhoAmI name="Rza" surname="Talibov" link="www.arrow.az" />
       <WhoAmI name="Vaqif" surname="Huseynov" link="www.vaqif.az" />
     </div>
-  )
+  );
 }
 
 export default App;
